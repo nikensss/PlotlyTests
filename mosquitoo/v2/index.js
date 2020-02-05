a03260ac27525c5dc34317c1ca233d6c50c668ea
nikensss <rikitzzz@gmail.com>
2020-02-06 00:49:26 +0100

    Minor changes

--- mosquitoo/v2/index.js
+++ mosquitoo/v2/index.js
@@ -1,9 +1,6 @@
 window.onload = function() {
-  const pulseWidths = [500e-9, 600e-9, 700e-9, 800e-9, 900e-9, 1e-6]; //6.0
-  const stepperPositions = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]; //10.0
-  const attempts = 10;
-
   const dataFilter = new DataFilter(data);
+  const attempts = Math.max.apply(null, dataFilter.getUnique('attempt'));
   const layout = {
     title: 'Measured pulse amplitude',
     scene: {
@@ -23,32 +20,30 @@ window.onload = function() {
   };
   const plotter = new Plotter(['plot-amplitude', 'plot-pw'], layout);
 
-  const layoutPulseAmplitude = {
-    title: 'Measured pulse amplitude',
-    scene: {
-      camera: { eye: { x: -0.55, y: -1.5, z: 1.7 } },
-      xaxis: { title: 'Stepper position' },
-      yaxis: { title: 'Pulse width' }
-    },
-    width: window.innerWidth * 0.45,
-    height: window.innerHeight * 0.88,
-    autosize: true,
-    xaxis: {
-      title: 'Stepper position'
-    },
-    yaxis: {
-      title: 'Pulse width'
-    }
-  };
-  const layoutPulseWidth = Object.assign({}, layoutPulseAmplitude);
-  layoutPulseWidth.title = 'Measured pulse width';
-
   console.log(data.length);
   const settings = {
     currentPercentage: 'Current Percentage',
     pulseWidth: 'Pulse width',
     stepperPosition: 'Stepper position'
   };
+
+  function buttonMarkUp(isFirst, dataTargetInt, dataTargetPrevious = '', dataTargetNext = '') {
+    return (
+      '<button class="btn btn-primary btn-target ' +
+      (isFirst ? 'btn-success' : '') +
+      '" data-target="target' +
+      dataTargetInt +
+      '" data-target-previous="' +
+      dataTargetPrevious +
+      '" data-target-next="' +
+      dataTargetNext +
+      '" data-target-int="' +
+      dataTargetInt +
+      '">' +
+      dataTargetInt +
+      '</button>'
+    );
+  }
 
   function btnTargetClick() {
     //highlight in green the currently selected button
@@ -60,15 +55,15 @@ window.onload = function() {
 
     const chosenSetting = $('.dropdown-toggle').attr('data-setting');
     const currentSettingValue = $(this).attr('data-target-int');
-    console.log(`Clicked button ${chosenSetting}: ${currentSettingValue}`);
+    console.log(`${chosenSetting}: ${currentSettingValue}`);
     const [yAxis, xAxis] = Object.keys(settings).filter(v => v !== chosenSetting);
     const [xValues, yValues] = [dataFilter.getUnique(xAxis), dataFilter.getUnique(yAxis)];
     console.log(xAxis + ': ' + xValues);
     console.log(yAxis + ': ' + yValues);
     //we get all the data for the current value of the selected setting
-    const firstFilter = dataFilter.get(chosenSetting, currentSettingValue);
 
     const [pulseAmplitudes, pulseWidths] = [[], []];
+    const firstFilter = dataFilter.get(chosenSetting, currentSettingValue);
     let secondFilter, thirdFilter, element, accPA, accPW;
     for (let y = 0; y < yValues.length; y++) {
       [pulseAmplitudes[y], pulseWidths[y]] = [[], []];
@@ -90,6 +85,8 @@ window.onload = function() {
       pulseAmplitudes,
       pulseWidths
     ]);
+    console.log('Plot finished!');
+    console.log('');
   }
 
   $('.btn-previous').click(function() {
@@ -125,34 +122,16 @@ window.onload = function() {
 
     //remove all buttons from buttons-banner
     $('.btn-target').remove();
-    const btnNext = $('.btn-next');
     //add the buttons needed to move through the values of the chosen setting
     buttons.forEach((v, i, a) => {
       const b = buttonMarkUp(i === 0, v, a[i - 1], a[i + 1]);
-      $(b).insertBefore(btnNext);
+      $(b).insertBefore($('.btn-next'));
     });
 
     $('.btn-target').click(btnTargetClick);
     $('.btn-success').click();
   });
 
+  //initiliase the plots
   $(".dropdown-button[data-setting='currentPercentage']").click();
 };
-
-function buttonMarkUp(isFirst, dataTargetInt, dataTargetPrevious = '', dataTargetNext = '') {
-  return (
-    '<button class="btn btn-primary btn-target ' +
-    (isFirst ? 'btn-success' : '') +
-    '" data-target="target' +
-    dataTargetInt +
-    '" data-target-previous="' +
-    dataTargetPrevious +
-    '" data-target-next="' +
-    dataTargetNext +
-    '" data-target-int="' +
-    dataTargetInt +
-    '">' +
-    dataTargetInt +
-    '</button>'
-  );
-}
